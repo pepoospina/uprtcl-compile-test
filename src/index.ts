d0738f34d19eca20e2fba4b5d05c0e9250f4c3eb
Pepo <pepo@uprtcl.io>
2021-04-11 16:59:40 +0200

    error recreate npm run build node /built/index.js

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
+import { CondensateCommits } from '@uprtcl/evees';
+import { StoreStub } from './stubs/store';
+
 console.log('Hello world');
+
+const store = new StoreStub();
+const a = new CondensateCommits(store, []);
+console.log(a);
